a29ad0c7ca1bc3b319297f67897bc315205df003
Aaron Heller <heller@ai.sri.com>
2018-08-21 13:59:02 -0700

    added acn2fuma_name()

--- acn_order.py
+++ acn_order.py
@@ -25,3 +25,22 @@ def acn2lm(channel_number):
     degree = floor(sqrt(channel_number)).astype(int)
     index = channel_number - degree**2 - degree
     return (degree, index)
+
+
+__fuma_channel_names = [u'W',
+                        u'Y', u'Z', u'X',
+                        u'V', u'T', u'R', u'S', u'U',
+                        u'Q', u'O', u'M', u'K', u'L', u'N', u'P']
+
+
+def acn2fuma_name(acn):
+    try:
+        name = __fuma_channel_names[acn]
+    except IndexError:
+        l, m = acn2lm(acn)
+        name = u'%d.%d' % (l, abs(m))
+        if m < 0:
+            name = name + u'S'
+        else:
+            name = name + u'C'
+    return name
