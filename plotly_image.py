6aaad779d2f16da9f6b10585371d4dad830dd028
Aaron Heller <heller@ai.sri.com>
2018-08-22 14:20:39 -0700

    plotly_image - further experiments with "flat" presentations of the performance metrics

--- plotly_image.py
+++ plotly_image.py
@@ -25,19 +25,55 @@ from grids import cart2sph, sph2cart
 
 import adt_scmd
 
+# this assumes you've aready run rVrE.py and some variables are left in the workspace
+#  (I know this is bad, but I'm just doing some experiments)
 
-flat_trace = go.Surface(x=az,
-                        y=el,
+flat_trace = go.Surface(name='rE',
+                        x=az * 180/np.pi,
+                        y=el * 180/np.pi,
                         z=np.zeros(np.shape(az)),
-                        cmin=0.5,
+                        cmin=0.7,
                         cmax=1,
                         surfacecolor=np.reshape(rEr, np.shape(az)),
-                        colorscale='Jet')
+                        colorscale='Jet',
+                        lighting=dict(ambient=1.0),
+                        hoverinfo='text',
+                        text=np.vectorize(
+                                lambda a, e, c:
+                                    "az: %.1f<br>el: %.1f<br> r<sub>E</sub>: %.2f" %
+                                    (a, e, c))
+                            (az * 180/np.pi, el * 180/np.pi,
+                             np.reshape(rEr, np.shape(az))))
+
+speakers = go.Scatter3d(name='Speakers (actual locations)',
+                     x=spkr_az * 180/np.pi,
+                     y=spkr_el * 180/np.pi,
+                     z=-0.01 + np.zeros(np.shape(spkr_az)),
+                     mode='markers',
+                     marker=dict(color='black'),
+                     hoverinfo='text',
+                     visible=True,
+                     text=np.vectorize(
+                             lambda a, e, r, c:
+                                 "%s<br>az: %.1f<br>el: %.1f<br> r: %.1f"
+                                 % (c, a, e, r))
+                             (spkr_az * 180/np.pi, spkr_el * 180/np.pi,
+                              spkr_rr, spkr_id))
+camera = dict(
+    up=dict(x=1, y=0, z=0),
+    center=dict(x=0, y=0, z=0),
+    eye=dict(x=0, y=0, z=-2))
 
 flat_layout = go.Layout(
+        showlegend=True,
+        legend=dict(orientation="h"),
         scene=dict(
-                aspectratio=dict(x=1, y=1, z=0.1),
+                xaxis=dict(title='azimuth (degrees)'),
+                yaxis=dict(title='elevation (degrees)'),
+                camera=camera,
+                aspectratio=dict(x=2, y=1, z=0.1),
                 zaxis=dict(range=(-0.1, 0.1))))
 
-plotly.offline.plot({'data': [flat_trace], 'layout': flat_layout},
+
+plotly.offline.plot({'data': [flat_trace, speakers], 'layout': flat_layout},
                     filename='flat.html')
