0ef3310d2c5939599aac086bfd596fa28f29fb72
Aaron Heller <heller@ai.sri.com>
2018-08-23 12:13:13 -0700

    plotly_image -- reformated code, some slight tinkering

--- plotly_image.py
+++ plotly_image.py
@@ -16,7 +16,6 @@ from plotly import tools as tls
 
 import matplotlib.pyplot as plt
 
-
 import real_spherical_harmonics as rsh
 import acn_order as acn
 
@@ -25,40 +24,45 @@ from grids import cart2sph, sph2cart
 
 import adt_scmd
 
-# this assumes you've aready run rVrE.py and some variables are left in the workspace
-#  (I know this is bad, but I'm just doing some experiments)
+# this assumes you've aready run rVrE.py and some variables are left in the
+# workspace (I know this is bad, but I'm just doing some experiments)
+#   az, el,
 
-flat_trace = go.Surface(name='rE',
-                        x=az * 180/np.pi,
-                        y=el * 180/np.pi,
-                        z=np.zeros(np.shape(az)),
-                        cmin=0.7,
-                        cmax=1,
-                        surfacecolor=np.reshape(rEr, np.shape(az)),
-                        colorscale='Jet',
-                        lighting=dict(ambient=1.0),
-                        hoverinfo='text',
-                        text=np.vectorize(
-                                lambda a, e, c:
-                                    "az: %.1f<br>el: %.1f<br> r<sub>E</sub>: %.2f" %
-                                    (a, e, c))
-                            (az * 180/np.pi, el * 180/np.pi,
-                             np.reshape(rEr, np.shape(az))))
+#rX = rEr; cmin=0.7; cmax=1
+rX = 180/np.pi * np.arccos(np.sum(rEu * xyz0, 0)); cmin=0; cmax=10;
 
-speakers = go.Scatter3d(name='Speakers (actual locations)',
-                     x=spkr_az * 180/np.pi,
-                     y=spkr_el * 180/np.pi,
-                     z=-0.01 + np.zeros(np.shape(spkr_az)),
-                     mode='markers',
-                     marker=dict(color='black'),
-                     hoverinfo='text',
-                     visible=True,
-                     text=np.vectorize(
-                             lambda a, e, r, c:
-                                 "%s<br>az: %.1f<br>el: %.1f<br> r: %.1f"
-                                 % (c, a, e, r))
-                             (spkr_az * 180/np.pi, spkr_el * 180/np.pi,
-                              spkr_rr, spkr_id))
+flat_trace = go.Surface(
+        name='rE',
+        x=az * 180/np.pi,
+        y=el * 180/np.pi,
+        z=np.zeros(np.shape(az)),
+        cmin=cmin,
+        cmax=cmax,
+        surfacecolor=np.reshape(rX, np.shape(az)),
+        colorscale='Jet',
+        lighting=dict(ambient=1.0),
+        hoverinfo='text',
+        text=np.vectorize(
+                lambda a, e, c:
+                "az: %.1f<br>el: %.1f<br> r<sub>E</sub>: %.2f" % (a, e, c))
+                (az * 180/np.pi,
+                 el * 180/np.pi,
+                 np.reshape(rX, np.shape(az))))
+
+speakers = go.Scatter3d(
+        name='Speakers',
+        x=spkr_az * 180/np.pi,
+        y=spkr_el * 180/np.pi,
+        z=-0.01 + np.zeros(np.shape(spkr_az)),
+        mode='markers',
+        marker=dict(color='blue'),
+        hoverinfo='text',
+        visible=True,
+        text=np.vectorize(
+                lambda a, e, r, c:
+                "%s<br>az: %.1f<br>el: %.1f<br> r: %.1f" % (c, a, e, r))
+                (spkr_az * 180/np.pi, spkr_el * 180/np.pi, spkr_rr, spkr_id))
+
 camera = dict(
     up=dict(x=1, y=0, z=0),
     center=dict(x=0, y=0, z=0),
