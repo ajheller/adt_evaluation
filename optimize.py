6019048b52010fc7fa50b186913c99d90d12b2c4
Aaron Heller <heller@ai.sri.com>
2020-08-11 17:38:27 -0700

    optimize.py: new implementation using scipy l-bgfs-b() and JAX for automatic differentiation

--- optimize.py
+++ optimize.py
@@ -6,95 +6,123 @@ Created on Tue Dec 31 02:48:26 2019
 @author: heller
 """
 
-# install pytorch with
-#    conda install pytorch torchvision -c pytorch
+# sanbox for optimizer using Jax for AutoGrad
+#   https://github.com/google/jax
 
-import torch
+#  conda create -n jax --clone base
+
+# jax installation:
+#   https://github.com/google/jax#installation
+
+# install jax with
+#   pip install jaxlib==0.1.51  # 0.1.52 won't run on MacOS 10.13
+#   pip install jax
+
+# Jax Autograd
+#  https://github.com/google/jax#automatic-differentiation-with-grad
+#  https://jax.readthedocs.io/en/latest/notebooks/autodiff_cookbook.html
+#  https://jax.readthedocs.io/en/latest/jax.html#automatic-differentiation
+# again, this only works on startup!
+from jax.config import config
+config.update("jax_enable_x64", True)
+
+
+import jax
+import jax.numpy as np  # jax overloads numpy
+import numpy as onp
+
+import scipy.optimize as opt
+
 import spherical_grids as sg
 import real_spherical_harmonics as rsh
 import basic_decoders as bd
 
+import localization_models as locm
 
-if False:
-    x = torch.rand(5, 3)
-    print(x)
 
-    T = sg.t_design5200()
 
-    Tt = torch.Tensor(T.u)
+# 3rd-order decoding
+l, m = zip(*rsh.lm_generator(3))
 
-    l, m = zip(*rsh.lm_generator(6))
 
-    Y = torch.Tensor(rsh.real_sph_harm_transform(l, m, T.az, T.el))
+# the test directions
+T = sg.t_design5200()
+Y = rsh.real_sph_harm_transform(l, m, T.az, T.el)
 
-    S240 = sg.t_design240()
-    Su = torch.Tensor(S240.u)
+# make a decoder matrix for the 240 speaker t-design
+S240 = sg.t_design240()
+Su = S240.u
+M = bd.inversion(l, m, S240.az, S240.el)
+M_shape = M.shape
 
-    M = torch.Tensor(bd.inversion(l, m, S240.az, S240.el))
 
+#@jax.jit(static_argnums=(1,2,3))
+def rE(M, Su, Y, full=False):
+    #G = M.reshape(M_shape) @ Y
     G = M @ Y
-
-    P = torch.sum(G, 0)
-    rV = (Su @ G) / P  # pytorch does the broadcasting
-
     G2 = G * G
+    E = np.sum(G2, axis=0)
+    rExyz = (Su @ G2) / E
+    if full:
+        rEr = np.linalg.norm(rExyz, ord=2, axis=0)
+        rEu = rExyz / rEr
+        return rExyz, rEu, rEr
+    else:
+        return rExyz
 
-    E = torch.sum(G2, 0)
+def loss(M, args):
+    return np.sum( (rE(M.reshape(args), Su, Y) - T.u * 0.75)**2 ) \
+            + np.sum(M**2)/1000  # regularization term
 
-    rE = (Su @ G2) / E
-
-    rEaz, rEel, rEr = sg.cart2sph(*rE)
-    rEu = sg.sph2cart(rEaz, rEel, 1)
-
-
-
-class TwoLayerNet(torch.nn.Module):
-    def __init__(self, D_in, H, D_out):
-        """
-        In the constructor we instantiate two nn.Linear modules and assign them as
-        member variables.
-        """
-        super(TwoLayerNet, self).__init__()
-        self.linear1 = torch.nn.Linear(D_in, H)
-        self.linear2 = torch.nn.Linear(H, D_out)
-
-    def forward(self, x):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Tensors.
-        """
-        h_relu = self.linear1(x).clamp(min=0)
-        y_pred = self.linear2(h_relu)
-        return y_pred
+val_and_grad_fn = jax.value_and_grad(loss)
 
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
+def loss_grad(M, args):
+    v, g = val_and_grad_fn(M, args)
+    # I'm not to happy about having to copy g but L-BGFS needs it in fortran
+    # order.  Check with g.flags
+    return v.copy(), onp.array(g, order='F') #onp.asfortranarray(g)
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
 
-# Construct our model by instantiating the class defined above
-model = TwoLayerNet(D_in, H, D_out)
 
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
-for t in range(500):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
+def rms_dir_error(M):
+    #print(M.shape)
+    G = M.reshape(M_shape) @ Y
+    G2 = G * G
+    E = np.sum(G2, axis=0)
+    rExyz = (Su @ G2) / E
 
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    if t % 100 == 99:
-        print(t, loss.item())
+    # magnitude and direction of rE
+    rEr = np.sqrt(np.sum(rExyz * rExyz, axis=0))
+    rEu = rExyz/rEr
 
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    # the direction error vector
+    rE_err_xyz = T.u - rEu
+    rE_err_mag = np.sum((rE_err_xyz * rE_err_xyz).ravel())/T.shape[0]
+
+
+    return rE_err_mag
+
+def gv(M):
+    v, g = jax.value_and_grad(rms_dir_error)(M)
+    return onp.asarray(v), onp.array(g, order='F')
+
+# https://docs.scipy.org/doc/scipy/reference/optimize.minimize-lbfgsb.html
+
+def o(M):
+    x, f, d = opt.fmin_l_bfgs_b(loss_grad, M.ravel(),
+                                args=(M.shape,),
+                                fprime=None,
+                                iprint=50)
+    M_opt = x.reshape(M.shape)
+
+    r = rE(M_opt, Su, Y, full=true)
+
+    return M_opt, f, d, r
+
+def o2(M):
+    return opt.fmin_l_bfgs_b(rms_dir_error, M, approx_grad=True, iprint=1)
+
+
+
+
