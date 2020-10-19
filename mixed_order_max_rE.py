73ee19ea478989e7459fc60b47d6b1fa0d86a4ec
Aaron Heller <heller@ai.sri.com>
2020-10-19 16:44:38 -0700

    flatten dictionary key go to 15th order

--- mixed_order_max_rE.py
+++ mixed_order_max_rE.py
@@ -19,7 +19,7 @@ s = esa.uniform240()
 
 M = dict()
 try:
-    for order_h in range(1, 10):
+    for order_h in range(1, 16):
         for order_v in range(1, order_h+1):
             for mos in ('HV', 'HP'):
                 key = (order_h, order_v, mos)
@@ -27,21 +27,20 @@ try:
                 c = pg.ChannelsAmbiX(order_h, order_v, mixed_order_scheme=mos)
 
                 M_pinv = bd.inversion(c.sh_l, c.sh_m, s.az, s.el)
-                M[key, 'pinv'] = M_pinv
+                M[(*key, 'pinv')] = M_pinv
 
                 M_allrad = bd.allrad(c.sh_l, c.sh_m, s.az, s.el)
-                M[key, 'allrad'] = M_pinv
+                M[(*key, 'allrad')] = M_pinv
                 try:
                     M_opt = odm.optimize(M_pinv, s.u.T, c.sh_l, c.sh_m)
                 except RuntimeError as rte:
                     print(rte)
-                    M[key, 'opt'] = rte
+                    M[(*key, 'opt')] = rte
                 else:
-                    M[key, 'opt'] = M_pinv
+                    M[(*key, 'opt')] = M_pinv
 
                 #lm.plot_performance(M_pinv, s.u.T, *c.sh(), title=key)
 
 
 finally:
-    print(key)
     pickle.dump(M, open('Mixed-Order-maxrE.pkl', 'wb'))
