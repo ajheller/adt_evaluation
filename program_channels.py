program_channels: fixed SN3D normalization robojo: updated to use fixed normalization

program_channels.py
@@ -81,16 +81,22 @@ import slugify as slug
 # I assume that the underlying real spherical harmonic code produces
 # full orthonormal values, hence these functions give the gains needed
 # to produce the target normalization from those
-# FIXME: this is the inverse of the normaliztion in the MATLAB ADT.
 
 
 def normalization_semi(sh_l, sh_m=None):
-    """gains to produce schmidt semi-normalized values from full orthronormal"""
-    return np.sqrt(2 * sh_l + 1)
+    """gains to produce schmidt semi-normalized (SN3D) values from full orthronormal (N3D)"""
+    return 1 / np.sqrt(2 * sh_l + 1)
 
 
 def normalization_full(sh_l, sh_m=None):
     return np.ones_like(sh_l, dtype=type(np.sqrt(1)))
+
+
+# real_spherical_harmonics produces fullly orthonormal values,
+#  i.e., the square integral over the full unit sphere is 1
+#  AmbiX specifies the square integral is 4pi, N3D doesn't say
+def normalization_unity_to_fourpi(sh_l, sh_m=None):
+    return np.full_like(sh_l, np.sqrt(4 * np.pi), dtype=type(np.sqrt(1)))
 
 
 # mixed-order sets
@@ -128,6 +134,7 @@ _FuMa_sh_acn = [l**2 + l + m for l, m in _FuMa_sh_lm]
 _FuMa_channel_names = np.array(tuple("W" + "XYZ" + "RSTUV" + "KLMNOPQ"))
 
 
+# these are the gains to produce FuMa from N3D
 _FuMa_channel_normalization = 1 / np.sqrt(
     np.array(
         (
@@ -410,7 +417,7 @@ class ChannelsAmbisonic(Channels):
         )
 
     def id_string(self, slugify=False):
-        return channels_id_string(self, slugify=True)
+        return channels_id_string(self, slugify=slugify)
 
     def __str__(self):
         return f"<Signal Set: {channels_id_string(self)}>"
@@ -562,7 +569,7 @@ def ambisonic_channels(C, convention=None, **kwargs):
     # use duck typing
     # does it behave like a ProgramChannels object?
     try:
-        return C.h_order, C.v_order, C.sh_l, C.sh_m, C.id_string()
+        return C.h_order, C.v_order, C.sh_l, C.sh_m, C.id_string(), C.normalization
     except AttributeError:
         pass
 

robojo.py
@@ -10,13 +10,14 @@ Created on Sun Dec 11 12:07:00 2022
 from gtts import gTTS  # pip install gtts
 
 import os
-
 import numpy as np
-from matplotlib import pyplot as plt
+
+# from matplotlib import pyplot as plt
 from scipy.io import wavfile as wav
 
 from functools import lru_cache
 
+# adt modules
 import program_channels as pc
 import real_spherical_harmonics as rsh
 
@@ -53,14 +54,16 @@ def robojo(
     if path is None:
         path = f"robojo-{C.id_string(slugify=True)}-el{elevation:+03d}-{language}.wav"
 
+    ho, vo, sh_l, sh_m, id_str, normalization = pc.ambisonic_channels(C)
+
     gains = rsh.real_sph_harm_transform(
-        C.sh_l, C.sh_m, np.array(azd) * np.pi / 180, np.array(eld) * np.pi / 180
+        sh_l, sh_m, np.array(azd) * np.pi / 180, np.array(eld) * np.pi / 180
     )
     x = np.zeros((gains.shape[0], len(text), dt * fs), dtype=np.int16)
 
     for i, t in enumerate(text):
         tw = text2speech(t, fs=fs, language=language)
-        for j, gain in enumerate(gains[:, i] / C.normalization * np.sqrt(4 * np.pi)):
+        for j, gain in enumerate(gains[:, i] * normalization * np.sqrt(4 * np.pi)):
             x[j, i, : len(tw)] = np.floor((tw * gain) + np.random.random(len(tw)))
 
     y = x.reshape(gains.shape[0], -1)
@@ -69,22 +72,28 @@ def robojo(
 
 
 def test(o, e):
-    C = pc.ChannelsAmbiX(o)
-    s = (
-        (0, e, "front"),
-        (-45, e, "front, right"),
-        (-90, e, "right"),
-        (-135, e, "back, right"),
-        (-180, e, "back"),
-        (135, e, "back, left"),
-        (90, e, "left"),
-        (45, e, "front, left"),
-        (0, 90, "top"),
-        (0, -90, "bottom"),
-    )
+    try:
+        # C = pc.ChannelsAmbiX(o)
+        # C = pc.ChannelsFuMa(o)
+        C = pc.ChannelsN3D(o)
+    except ValueError as ve:
+        print(ve)
+    else:
+        s = (
+            (0, e, "front"),
+            (-45, e, "front, right"),
+            (-90, e, "right"),
+            (-135, e, "back, right"),
+            (-180, e, "back"),
+            (135, e, "back, left"),
+            (90, e, "left"),
+            (45, e, "front, left"),
+            (0, 90, "top"),
+            (0, -90, "bottom"),
+        )
 
-    azd, eld, text = zip(*s)
-    return robojo(C, text, azd, eld)
+        azd, eld, text = zip(*s)
+        return robojo(C, text, azd, eld)
 
 
 def test2(o, es):